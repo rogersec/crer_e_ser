c1b32d2990f718176bbe051e4f1a08cb1f802e13
Jessica Paz <jessicamorim.42@gmail.com>
2017-10-19 12:13:13 -0300

    Setup project

---
+++ crer_e_ser/fichas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FichasConfig(AppConfig):
+    name = 'fichas'
---
+++ crer_e_ser/fichas/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def odonto_form(request):
+    return render(request, 'odonto_form.html')
