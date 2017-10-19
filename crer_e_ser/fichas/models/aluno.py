dd390026066137258a2906f1bc2ea2dca86d9e74
= <rogersec42@gmail.com>
2017-10-19 20:11:04 -0300

    modificacao nos models

--- crer_e_ser/fichas/models/aluno.py
+++ crer_e_ser/fichas/models/aluno.py
@@ -4,13 +4,16 @@ from django.utils import timezone
 class Aluno(models.Model):
     nome = models.CharField(blank=False, max_length=200)
     idade = models.IntegerField(blank=False)
-    nome_responsavel = models.CharField(blank=False)
-    contato = models.CharField(blank=False)
+    nome_responsavel = models.CharField(blank=False, max_length=200)
+    contato = models.CharField(blank=False, max_length=11)
     data_de_atendimento = models.DateField(blank=False, default=timezone.now)
 
-    def __init__(self, nome, idade, nome_responsavel, contato, data_de_atendimento):
+    def __init__(self, nome, idade, nome_responsavel, contato):
         self.nome = nome
         self.idade = idade
         self.nome_responsavel = nome_responsavel
         self.contato = contato
+
+    def __str__(self):
+        return '<Aluno> %s' % (self.nome)
 
