302c9c399b76910088f65d99cde5300e602d3d65
= <rogersec42@gmail.com>
2017-10-20 13:36:40 -0300

    atualizar o model odonto.py

--- crer_e_ser/fichas/models/odonto.py
+++ crer_e_ser/fichas/models/odonto.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Ficha(models.Model):
+    escovacao_diaria = models.CharField(max_length=20, blank=False)
+    escova = models.CharField(max_length=20, blank=False)
+    troca_de_escova = models.CharField(max_length=20, blank=False)
+    usa_creme_dental = models.BooleanField(blank=False)
+    foi_ao_dentista = models.BooleanField(blank=False)
+
+
+
+    
